b3ad44a5b73e0339ccca97839a008c8bfe73ba25
dav-dev-codes <davilopesr2244@gmail.com>
2025-04-07 16:11:50 -0300

    :tada: inicial commit

---
+++ viagem/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('home/', include('home.urls')),
+    path('cadastro/', include('cadastro.urls')),
+    path('destinos/', include('destinos.urls')),
+    path('fotos/', include('fotos.urls')),
+    path('blog/', include('blog.urls')),
+]
